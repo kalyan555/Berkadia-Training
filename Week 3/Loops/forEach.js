b17caecb3936f699ba3001d0d769c8df2db48ecf
M.V.K.Kalyan Reddy <kalyankrishna01@gmail.com>
2018-11-30 11:39:11 +0530

    Create forEach.js

---
+++ Week 3/Loops/forEach.js
@@ -0,0 +1,18 @@
+(function(){
+   var arr=[1,2,9,4,1,8];
+    arr.forEach(function(element) {
+        console.log(element);
+      });
+})();
+
+/*
+Output
+-------
+node forEach.js
+1
+2
+9
+4
+1
+8
+*/

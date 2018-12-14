102735ddaa9d62b500ca3328a9119748285a3282
M.V.K.Kalyan Reddy <kalyankrishna01@gmail.com>
2018-12-14 15:06:04 +0530

    Create express_json.js

---
+++ Week5/Nodejs/Express Documentation/express/express_json.js
@@ -0,0 +1,64 @@
+let express = require("express");
+let app = express();
+//Create a middle ware aplication which can be used with next() method 
+
+
+// next method call the continuation of the program after that function in the below app.get()
+app.get('/', (req, res, next) => {
+    res.end("Hello World");
+    console.log("expression.json() function: \n"+express.json());
+});
+app.listen(3000, () => console.log("listenning"));
+
+/*
+The detailed output of the json function in given which hold the values in the form of key value pair
+Output:
+--------
+node app.js
+listenning
+expression.json() function:
+function jsonParser (req, res, next) {
+    if (req._body) {
+      debug('body already parsed')
+      next()
+      return
+    }
+
+    req.body = req.body || {}
+
+    // skip requests without bodies
+    if (!typeis.hasBody(req)) {
+      debug('skip empty body')
+      next()
+      return
+    }
+
+    debug('content-type %j', req.headers['content-type'])
+
+    // determine if request should be parsed
+    if (!shouldParse(req)) {
+      debug('skip parsing')
+      next()
+      return
+    }
+
+    // assert charset per RFC 7159 sec 8.1
+    var charset = getCharset(req) || 'utf-8'
+    if (charset.substr(0, 4) !== 'utf-') {
+      debug('invalid charset')
+      next(createError(415, 'unsupported charset "' + charset.toUpperCase() + '"', {
+        charset: charset,
+        type: 'charset.unsupported'
+      }))
+      return
+    }
+
+    // read
+    read(req, res, next, parse, debug, {
+      encoding: charset,
+      inflate: inflate,
+      limit: limit,
+      verify: verify
+    })
+  }
+*/

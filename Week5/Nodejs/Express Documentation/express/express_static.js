e00f243b16f1a82c16eabd1328bce5110132fe21
M.V.K.Kalyan Reddy <kalyankrishna01@gmail.com>
2018-12-14 15:13:06 +0530

    Create express_static.js

---
+++ Week5/Nodejs/Express Documentation/express/express_static.js
@@ -0,0 +1,74 @@
+let express = require("express");
+let app = express();
+//Create a middle ware aplication which can be used with next() method 
+
+
+// next method call the continuation of the program after that function in the below app.get()
+app.get('/', (req, res, next) => {
+    res.end("Hello World");
+    console.log("\n\nexpression.static() function: \n"+express.static("public"));
+});
+app.listen(3000, () => console.log("listenning"));
+
+
+/*
+OUTPUT:
+-------
+node app.js
+expression.static() function:
+function serveStatic (req, res, next) {
+    if (req.method !== 'GET' && req.method !== 'HEAD') {
+      if (fallthrough) {
+        return next()
+      }
+
+      // method not allowed
+      res.statusCode = 405
+      res.setHeader('Allow', 'GET, HEAD')
+      res.setHeader('Content-Length', '0')
+      res.end()
+      return
+    }
+
+    var forwardError = !fallthrough
+    var originalUrl = parseUrl.original(req)
+    var path = parseUrl(req).pathname
+
+    // make sure redirect occurs at mount
+    if (path === '/' && originalUrl.pathname.substr(-1) !== '/') {
+      path = ''
+    }
+
+    // create send stream
+    var stream = send(req, path, opts)
+
+    // add directory handler
+    stream.on('directory', onDirectory)
+
+    // add headers listener
+    if (setHeaders) {
+      stream.on('headers', setHeaders)
+    }
+
+    // add file listener for fallthrough
+    if (fallthrough) {
+      stream.on('file', function onFile () {
+        // once file is determined, always forward error
+        forwardError = true
+      })
+    }
+
+    // forward errors
+    stream.on('error', function error (err) {
+      if (forwardError || !(err.statusCode < 500)) {
+        next(err)
+        return
+      }
+
+      next()
+    })
+
+    // pipe
+    stream.pipe(res)
+  }
+*/
